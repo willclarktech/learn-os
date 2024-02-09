c705c1856033f96906bbd1a06ec118af588b5422
willclarktech <willclarktech@users.noreply.github.com>
2024-02-09 20:12:09 +0100

    Add a very simple ASM kernel

---
+++ src/kernel.asm
@@ -0,0 +1,35 @@
+; Interrupt code for performing video actions
+; ah: The action to perform
+INT_VIDEO equ 0x10
+; Video action to print a character
+; al: The character to print
+INT_VIDEO_PRINT_CHAR equ 0x0e
+
+CHAR_TERMINATOR equ 0x00
+
+start:
+	; TODO: Explain these two lines
+	mov ax, cs
+	mov ds, ax
+
+	mov si, hello_string
+	call print_string
+
+	jmp $ ; Infinite loop
+
+; Prints a string
+; si: The start address of the string to print
+print_string:
+	mov ah, INT_VIDEO_PRINT_CHAR
+
+	print_next_char:
+		lodsb
+		cmp al, CHAR_TERMINATOR
+		je printing_finished
+		int INT_VIDEO
+		jmp print_next_char
+
+	printing_finished:
+		ret
+
+hello_string db "Hello from the kernel!", CHAR_TERMINATOR
