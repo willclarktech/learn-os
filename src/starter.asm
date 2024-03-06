b07ea62ec7b2f3511abf925cc053faaa5910ee60
willclarktech <willclarktech@users.noreply.github.com>
2024-03-06 20:40:59 +0100

    Update kernel starter for interrupts

--- src/starter.asm
+++ src/starter.asm
@@ -1,7 +1,8 @@
-bits 16
 extern kernel_main
+extern interrupt_handler
 
 ; Offsets in GDT
+NULL_SEGMENT equ 0x00
 KERNEL_CODE_SEGMENT equ 0x08
 KERNEL_DATA_SEGMENT equ 0x10
 
@@ -17,6 +18,29 @@ INT_VIDEO_SET_CURSOR_TYPE equ 0x01
 
 VIDEO_MODE_TEXT equ 0x03
 CURSOR_TYPE_DISABLED equ 0x2000
+PIC_PORT_PRIMARY_COMMAND equ 0x20
+PIC_PORT_PRIMARY_DATA equ 0x21
+PIC_PORT_SECONDARY_COMMAND equ 0xa0
+PIC_PORT_SECONDARY_DATA equ 0xa1
+
+; Command to initialize the programmable interrupt controller (PIC)
+; Send this to the command port and then send the following parameters to the data port:
+; 1. New starting offset of IRQs
+; 2. Number of the slot connected to the other PIC (primary/secondary)
+; 3. Which mode (eg x86)
+; 4. Which IRQs to enable/disable
+PIC_COMMAND_INIT equ 0x11
+PIC_OFFSET_PRIMARY equ 0x20
+PIC_OFFSET_SECONDARY equ 0x28
+PIC_SLOT_PRIMARY equ 0x04 ; 2^2
+PIC_SLOT_SECONDARY equ 0x02
+PIC_MODE_X86 equ 0x01
+PIC_ENABLE_IRQS_ALL equ 0x00
+
+; Command to inform the PIC of the end of an interrupt
+PIC_COMMAND_EOI equ 0x20
+
+bits 16
 
 start:
 	; Set the data segment to equal the current code segment
@@ -53,8 +77,40 @@ enter_protected_mode:
 	mov cr0, eax
 	ret
 
-; TODO: Fill this out
 setup_interrupts:
+	call remap_pic
+	call load_idt
+	ret
+
+remap_pic:
+	mov al, PIC_COMMAND_INIT
+	out PIC_PORT_PRIMARY_COMMAND, al
+	out PIC_PORT_SECONDARY_COMMAND, al
+
+	mov al, PIC_OFFSET_PRIMARY
+	out PIC_PORT_PRIMARY_DATA, al
+
+	mov al, PIC_OFFSET_SECONDARY
+	out PIC_PORT_SECONDARY_DATA, al
+
+	mov al, PIC_SLOT_PRIMARY
+	out PIC_PORT_PRIMARY_DATA, al
+
+	mov al, PIC_SLOT_SECONDARY
+	out PIC_PORT_SECONDARY_DATA, al
+
+	mov al, PIC_MODE_X86
+	out PIC_PORT_PRIMARY_DATA, al
+	out PIC_PORT_SECONDARY_DATA, al
+
+	mov al, PIC_ENABLE_IRQS_ALL
+	out PIC_PORT_PRIMARY_DATA, al
+	out PIC_PORT_SECONDARY_DATA, al
+
+	ret
+
+load_idt:
+	lidt [idtr - start]
 	ret
 
 bits 32
@@ -65,8 +121,8 @@ start_kernel:
 	mov ds, eax
 	mov ss, eax
 
-	; Null value because these segments won't be used
-	mov eax, 0x00
+	; Null because these segments won't be used
+	mov eax, NULL_SEGMENT
 	mov es, eax
 	mov fs, eax
 	mov gs, eax
@@ -76,3 +132,4 @@ start_kernel:
 	call kernel_main
 
 %include "src/gdt.asm"
+%include "src/idt.asm"
