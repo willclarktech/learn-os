05600ea136f84b2005993f586bf9f55c31c44434
willclarktech <willclarktech@users.noreply.github.com>
2024-03-07 10:46:48 +0100

    Simplify bootloader

--- src/bootstrap.asm
+++ src/bootstrap.asm
@@ -11,6 +11,8 @@ KERNEL_SECTOR equ 0x02 ; Right after the boot loader
 KERNEL_SEGMENT equ 0x0900
 KERNEL_START equ 0x0000
 
+NUM_SECTORS_TO_LOAD equ 0x0f
+
 ; Interrupt code for performing video actions
 ; ah: The action to perform
 INT_VIDEO equ 0x10
@@ -23,6 +25,11 @@ INT_VIDEO_READ_CURSOR equ 0x03
 ; Video action to print a character
 ; al: The character to print
 INT_VIDEO_PRINT_CHAR equ 0x0e
+
+DEFAULT_PAGE_NUMBER equ 0x00
+START_OF_LINE equ 0x00
+CHAR_TERMINATOR equ 0x00
+CHAR_NEWLINE equ 0x0a
 
 ; Interrupt code for performing HDD actions
 ; ah: The action to perform
@@ -38,13 +45,9 @@ INT_HDD equ 0x13
 INT_HDD_READ equ 0x02
 
 HARD_DISK_0 equ 0x80
-
-CHAR_TERMINATOR equ 0x00
-CHAR_NEWLINE equ 0x0a
-
-DEFAULT_PAGE_NUMBER equ 0x00
-
-START_OF_LINE equ 0x00
+DEFAULT_TRACK equ 0x00
+DEFAULT_HEAD equ 0x00
+DEFAULT_OFFSET equ 0x00
 
 start:
 	; Set the data segment to the start of the bootloader
@@ -88,35 +91,19 @@ print_string:
 		ret
 
 load_kernel_from_disk:
-	N_SECTORS equ 0x01
-	TRACK equ 0x00
-	HEAD equ 0x00
-
 	mov ax, KERNEL_SEGMENT
 	mov es, ax
 
-	mov ax, [current_sector_to_load]
-	sub ax, KERNEL_SECTOR ; Remove the sector offset from the RAM target
-	mov bx, SECTOR_SIZE
-	mul bx ; Multiply ax by bx
-	mov bx, ax
-
 	mov ah, INT_HDD_READ
-	mov al, N_SECTORS
-	mov ch, TRACK
-	mov cl, [current_sector_to_load]
-	mov dh, HEAD
+	mov al, NUM_SECTORS_TO_LOAD
+	mov ch, DEFAULT_TRACK
+	mov cl, KERNEL_SECTOR
+	mov dh, DEFAULT_HEAD
 	mov dl, HARD_DISK_0
+	mov bx, DEFAULT_OFFSET
 	int INT_HDD
 
 	jc kernel_load_error
-
-	; TODO: Can we replace this loop by just setting N_SECTORS?
-	; TODO: Can we remove the `byte`s here?
-	inc byte [current_sector_to_load]
-	dec byte [num_sectors_to_load]
-	jnz load_kernel_from_disk
-
 	ret
 
 kernel_load_error:
@@ -127,9 +114,6 @@ kernel_load_error:
 title_string db "Welcome to the bootloader", CHAR_TERMINATOR
 message_string db "Loading kernel...", CHAR_TERMINATOR
 load_error_string db "Failed to load kernel", CHAR_TERMINATOR
-
-current_sector_to_load db KERNEL_SECTOR
-num_sectors_to_load db 0x0f
 
 ; Pad with null data
 times (BOOTLOADER_SIZE-BOOTLOADER_MAGIC_BYTES_SIZE)-($-$$) db 0
